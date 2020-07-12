9896d85eeaba46535b567ac65291d5e3634cb9c7
adrianhorn <64524954+adrianhorn@users.noreply.github.com>
2020-07-12 17:16:14 -0500

    Update juego.js

--- juego.js
+++ juego.js
@@ -1,18 +1,24 @@
-var muerte,jugar,vidaEnemigo,tiempo,mana;
-var name, textoBienvenida,turnoEnemigo
-var vida, ataque, curacion,
 
+var muerte,jugar,vidaEnemigo,tiempo,mana;
+var name, textoBienvenida,turnoEnemigo;
+var vida, ataque, curacion, golpeEnemigo;
+var heroe, enemigo;
 //estadisticas
 mana=50
 turnoEnemigo=false
 ataque = -20;
 tiempo = 0;
-vidaEnemigo=80;
-vida   = 100;
-curacion=30;
-muerte=false
+vidaEnemigo=200;
+golpeEnemigo=-15;
+vida   = 200;
+curacion=45;
+muerte=false;
+enemigo=document.getElementById("enemigo");
+heroe=document.getElementById("heroe");
 textoBienvenida='Hola ' + name;
-name='Aventurero'
+name='Aventurero';
+
+
 
 
 //tipos de ataques
@@ -41,10 +47,18 @@ function quemar(){
 
 //ataque Enemigo
 function AtaqueEnemigo(){
-	if (1){
-			vida+=GolpeEnemigo
-		}
-}
+		 	document.getElementById("Encendiar").disabled = true;
+			document.getElementById("Jugar").disabled = true;
+			document.getElementById("curacion").disabled = true;
+	  setTimeout(
+	    function() {
+				document.getElementById("Encendiar").disabled =false;
+				document.getElementById("Jugar").disabled = false;
+				document.getElementById("curacion").disabled = false;
+vida+=golpeEnemigo;
+}, 1500);
+	}
+
 
 
 //botones
@@ -57,10 +71,13 @@ function juega(){
 	if(dado==1){
 		golpe();
 	}
-	}
+}
 function curarse(){
 	mana+=curacion;
 	vida+=curacion;
+	if(vida>200){
+		vida=200
+	}
 	document.getElementById("Encendiar").disabled = false;
 	textoColor()
 	if (vida > 0){
@@ -70,6 +87,7 @@ function curarse(){
 		muerte = true;
 		perder();
 		}
+		AtaqueEnemigo();
 }
 
 
@@ -92,24 +110,36 @@ function saludar(){
 
 //estado
 function perder(){
-	textoColor(" morido","red");
-	document.getElementById("quemar").disabled = true;
-	document.getElementById("golpe").disabled = true;
-	document.getElementById("fallar").disabled = true;
+	if (vida<0){
+		vida=0;
+		textoColor(" morido","red");
+		document.getElementById("quemar").disabled = true;
+		document.getElementById("golpe").disabled = true;
+		document.getElementById("fallar").disabled = true;
+	}
 }
 function textoColor(texto="",color="Blue"){
 	document.getElementById("estado").innerHTML=name+"<br>"+texto;
 	document.getElementById("estado").style.color= color;
 }
+function mover(){
+			heroe.style.left='"100px"';
+}
+
+//	for(k=250;k>0;k--){
+	//		enemigo.style.right='+=10px';
 
 function revision(){
-	if (vida>250){
-		vida=250;
+	if (vida>200){
+		vida=200;
 	}
+	perder();
+	heroe.height=vida*1.25;
+	enemigo.height=vidaEnemigo*1.3;
 	document.getElementById("tiempo").innerHTML=tiempo;
 	document.getElementById("tiempo").style.color= "purple";
 	document.getElementById("cantidad de vida").style.color="lime"
-	document.getElementById("cantidad de vida").innerHTML=vida + '/250 de vida';
+	document.getElementById("cantidad de vida").innerHTML=vida + '/200 de vida';
 	document.getElementById("cantidad de vida del enemigo").style.color="maroon"
 	document.getElementById("cantidad de vida del enemigo").innerHTML=vidaEnemigo + ' de vida del Enemigo';
 }
@@ -121,4 +151,4 @@ setInterval(function(){
 }, 1000);
 setInterval(function(){
 	revision ();
-}, 250);
+}, 100);
