1a31290079550ad88624892304e5f889e62dfdbf
adrianhorn <64524954+adrianhorn@users.noreply.github.com>
2020-07-14 10:17:09 -0500

    Update juego.js

--- juego.js
+++ juego.js
@@ -1,141 +1,156 @@
-
-var muerte,jugar,vidaEnemigo,tiempo,mana;
-var name, textoBienvenida,turnoEnemigo;
+var muerte,jugar,vidaEnemigo,tiempo,pocion;
+var name,turnoEnemigo;
 var vida, ataque, curacion, golpeEnemigo;
-var heroe, enemigo;
+var heroe, enemigo,deshabilitarenemigo;
+
+
 //estadisticas
-mana=50
 turnoEnemigo=false
-ataque = -20;
+ataque = -25;
 tiempo = 0;
+pocion = 6;
 vidaEnemigo=200;
-golpeEnemigo=-15;
+golpeEnemigo=-20;
 vida   = 200;
+deshabilitarenemigo= 0;
 curacion=45;
 muerte=false;
 enemigo=document.getElementById("enemigo");
 heroe=document.getElementById("heroe");
 textoBienvenida='Hola ' + name;
-name='Aventurero';
+name='yoshi';
 
 
-
-
-//tipos de ataques
-function fallar(){
-	textoColor(" falló😪",'#4EFAC0');
-	turnoEnemigo=true
-	document.getElementById("flama").style.display = "none";
-	AtaqueEnemigo()
-}
-function golpe(){
-	textoColor(" atacó👊",'#4EFAC0');
-	vidaEnemigo+=ataque;
-	turnoEnemigo=true;
-	document.getElementById("flama").style.display = "none";
-	AtaqueEnemigo()
-}
-function quemar(){
-			vidaEnemigo+=ataque;
-			mana-15;
-			turnoEnemigo=true
-			textoColor(" quemó al Enemigo",'#4EFAC0');
-			document.getElementById("flama").style.display = "block";
-			AtaqueEnemigo()
+//nombre
+function nombrarse(){
+		name=prompt('¿Como te llamas?');
+		if (!name){
+			name="yoshi ";
 		}
+		console.log(name);
+		return(name);
+		textocolor();
+		document.getElementById("estado").innerHTML=name;
+}
+nombrarse();
 
 
-//ataque Enemigo
-function AtaqueEnemigo(){
-		 	document.getElementById("Encendiar").disabled = true;
+//Movimientos Enemigo
+function MovimientosEnemigo(){
+		revision()
+	setTimeout{
+		if (deshabilitarenemigo== 0){
 			document.getElementById("Jugar").disabled = true;
 			document.getElementById("curacion").disabled = true;
-	  setTimeout(
-	    function() {
-				document.getElementById("Encendiar").disabled =false;
 				document.getElementById("Jugar").disabled = false;
 				document.getElementById("curacion").disabled = false;
-vida+=golpeEnemigo;
-}, 1500);
+		}
+	},1200;
+}
+function lonk(){
+	if(vida<80){
+		var dado = Math.floor(Math.random() *3);
+		if(dado==0){
+			vidaEnemigo+=35;
+		}
+		if(dado==1){
+			vida+=golpeEnemigo;
+		}
+		if(dado==2){
+			vida+=golpeEnemigo;
+		}
+		if(dado==3){
+		}
 	}
-
+	else var dado = Math.floor(Math.random() *2);
+		if (dado==0){
+			vida+=golpeEnemigo;
+		}
+		if (dado==1){
+			vida+=golpeEnemigo;
+		}
+		if (dado==2){}
+}
 
 
 //botones
 function juega(){
-	textoColor()
-	var dado = Math.floor(Math.random() *2);
+	var dado = Math.floor(Math.random() *4);
 	if(dado==0){
 		fallar();
 	}
 	if(dado==1){
 		golpe();
 	}
+	if(dado==2){
+		golpe();
+	}
+	if(dado==3){
+		golpe();
+	}
 }
 function curarse(){
-	mana+=curacion;
-	vida+=curacion;
-	if(vida>200){
-		vida=200
-	}
-	document.getElementById("Encendiar").disabled = false;
-	textoColor()
-	if (vida > 0){
-		muerte=false;
-		textoColor(" curado","green");
-	}else{
-		muerte = true;
-		perder();
+		if(pocion==0){
+			document.getElementById('curacion').disabled=true
 		}
-		AtaqueEnemigo();
+		else{
+			pocion -=1
+			revision()
+			vida+=curacion;
+			if(vida>200){
+				vida=200
+			}
+			textoColor(" curado","green");
+		}
+	perder();
+
+	MovimientosEnemigo();
 }
 
 
-//nombre
-function nombrarse() {
-		name=prompt('¿Como te llamas?');
-		if (!name){
-			name="desconocido ";
-		}
-		textoColor()
-		console.log(name);
-		return(name);
+//tipos de ataques
+function fallar(){
+	textoColor(" falló😪",'#4EFAC0');
+	turnoEnemigo=true
+	MovimientosEnemigo()
 }
-function saludar(){
-	textoColor()
-	textoBienvenida='Hola ' + name;
-	document.getElementById("estado").innerHTML=name;
+function golpe(){
+	textoColor(" atacó👊",'#4EFAC0');
+	vidaEnemigo+=ataque;
+	turnoEnemigo=true;
+	MovimientosEnemigo()
 }
 
 
 //estado
-function perder(){
-	if (vida<0){
-		vida=0;
-		textoColor(" morido","red");
-		document.getElementById("quemar").disabled = true;
-		document.getElementById("golpe").disabled = true;
-		document.getElementById("fallar").disabled = true;
+function ganar(){
+	if (vidaEnemigo<1){
+		vidaEnemigo=0;
+		deshabilitarenemigo= 1;
+		textoColor(" ganó","yellow");
+		document.getElementById("Jugar").disabled = true;
+		document.getElementById("curacion").disabled = true;
 	}
 }
-function textoColor(texto="",color="Blue"){
-	document.getElementById("estado").innerHTML=name+"<br>"+texto;
-	document.getElementById("estado").style.color= color;
-}
-function mover(){
-			heroe.style.left='"100px"';
+function perder(){
+	if (vida<1){
+		vida=0;
+		textoColor(" morido","red");
+		document.getElementById("curacion").disabled = true;
+		document.getElementById("Jugar").disabled = true;
+	}
 }
 
-//	for(k=250;k>0;k--){
-	//		enemigo.style.right='+=10px';
 
 function revision(){
 	if (vida>200){
 		vida=200;
 	}
 	perder();
-	heroe.height=vida*1.25;
-	enemigo.height=vidaEnemigo*1.3;
+	ganar();
+	document.getElementById("curacion").innerHTML='Usar Pocion '+ pocion;
+//heroe.height=vida*2;
+//enemigo.height=vidaEnemigo*2;
 	document.getElementById("tiempo").innerHTML=tiempo;
 	document.getElementById("tiempo").style.color= "purple";
 	document.getElementById("cantidad de vida").style.color="lime"
@@ -143,9 +158,13 @@ function revision(){
 	document.getElementById("cantidad de vida del enemigo").style.color="maroon"
 	document.getElementById("cantidad de vida del enemigo").innerHTML=vidaEnemigo + ' de vida del Enemigo';
 }
+function textoColor(texto="",color="Blue"){
+	document.getElementById("estado").innerHTML=name+"<br>"+texto;
+	document.getElementById("estado").style.color= color;
+}
+
 
 //bucles
-
 setInterval(function(){
 	tiempo++;
 }, 1000);
